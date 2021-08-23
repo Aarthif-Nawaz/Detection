76f4e99c612b73fc33b440bb349c0d3953805faa
Aarthif-Nawaz <mohammed.2017313@iit.ac.lk>
2021-08-22 17:10:02 -0800

    Push

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import cv2
 import easyocr
 import warnings
 import shutil
+import imutils
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 warnings.filterwarnings("ignore")
@@ -15,13 +17,16 @@ app.config['SECRET_KEY'] = "asadbsjdnsfkslnkslfnmksdfmnaskldasd"
 @app.route('/api/getData', methods=['GET', 'POST'])
 def readData():
     if request.method == "POST":
-        filename_data = request.files['passport']
+        try:
+            filename_data = request.files['passport']
+        except:
+            return jsonify({'Result': 'Please Provide Passport as a key and the Image as Value !'})
         shutil.rmtree('Passport')
         os.mkdir('Passport')
-        filename_data.save(os.path.join(os.getcwd(),f'Passport/{filename_data.filename}'))
+        filename_data.save(os.path.join(os.getcwd(), f'Passport/{filename_data.filename}'))
         try:
             reader = easyocr.Reader(['en'], gpu=True)
-            bounds = reader.readtext(os.path.join(os.getcwd(),f'Passport/{filename_data.filename}'))
+            bounds = reader.readtext(os.path.join(os.getcwd(), f'Passport/{filename_data.filename}'))
             for i in bounds:
                 if str(i[1]).__contains__("<"):
                     passport = str(i[1]).split("<")
@@ -31,11 +36,31 @@ def readData():
                         name = ' '.join([str(elem) for elem in name])
                         country = passport[1][0:3]
                         if name.__contains__(country):
-                            name = name.replace(country,'')
+                            name = name.replace(country, '')
                         return jsonify({
                             'Name': name,
                             'Country': country
                         })
+            image = cv2.imread(os.path.join(os.getcwd(), f'Passport/{filename_data.filename}'))
+            rotated = imutils.rotate_bound(image, 270)
+            reader = easyocr.Reader(['en'], gpu=True)
+            bounds = reader.readtext(rotated)
+            for i in bounds:
+                if str(i[1]).__contains__("<"):
+                    passport = str(i[1]).split("<")
+                    passport = list(filter(None, passport))
+                    if passport[0] == "P":
+                        name = passport[-3:]
+                        name = ' '.join([str(elem) for elem in name])
+                        country = passport[1][0:3]
+                        if name.__contains__(country):
+                            name = name.replace(country, '')
+                        return jsonify(
+                            {
+                                'Name': name,
+                                'Country': country
+                            }
+                        )
             return jsonify({'Result': 'Please Provide A Clear Image Of Passport'})
         except Exception as e:
             print(e)
